dac2ba0bc2679db793f9852238f344f46ab6c601
Rus <de.mr44@gmail.com>
2021-10-06 18:49:11 +0500

    Initial commit

---
+++ MVC-Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Code
+//
+//  Created by ruslan on 05.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
